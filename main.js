36eb502828042cb13232b233827de354b1258366
Giovanni Lucas <giovannilucasmoura@gmail.com>
2020-11-05 21:52:03 -0300

    Finalização de JavaScript - Jogo Funcional

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const cards = document.querySelectorAll('.card');
-let hasFLippedCard = false;
+let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 function flipCard() {
+   if(lockBoard) return;
+   if(this === firstCard) return;
+
    this.classList.toggle('flip');
 
-   if(!hasFLippedCard) {
-      hasFLippedCard = True;
+   if(!hasFlippedCard) {
+      hasFlippedCard = true;
       firstCard = this;
       return;
    }
@@ -23,7 +27,38 @@ function checkForMath() {
 
    unflipCards();
 }
+
+function disableCards() {
+   firstCard.removeEventListener('click', flipCard);
+   secondCard.removeEventListener('click', flipCard);
+
+   resetBoard();
+}
+
+function unflipCards() {
+   lockBoard = true;
+
+   setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+
+      resetBoard();
+   }, 1500);
+}
+
+function resetBoard() {
+   [hasFlippedCard, lockBoard] = [false, false];
+   [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+   cards.forEach((card) => {
+      let randomPosition = Math.floor(Math.random() * 12);
+
+      card.style.order = randomPosition;
+   });
+})();
+
 cards.forEach((card) => {
    card.addEventListener('click', flipCard);
-
 });
